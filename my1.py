083653b71857d4bdadedaad0b211e3ed8511f6e9
kul-aero <kulagin.ale@gmail.com>
2025-08-29 21:41:07 +0300

    Last commit

--- my1.py
+++ my1.py
@@ -2,7 +2,7 @@ def sum_of_two_numbers(a, b):
     return a + b
 
 # Example usage
-num1 = 5
+num1 = 88
 num2 = 7
 result = sum_of_two_numbers(num1, num2)
-print(f" Сумма 2х чисел  {num1} and {num2} is {result}")
+print(f" Сумма 2х  чисел  {num1} and {num2} is {result}")
