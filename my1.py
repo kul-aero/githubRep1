954edc9d8663b96f2900bbaca9da650c746b6f73
kul-aero <kulagin.ale@gmail.com>
2025-09-01 18:31:28 +0300

    update my1.py on VSCode

--- my1.py
+++ my1.py
@@ -5,4 +5,4 @@ def sum_of_two_numbers(a, b):
 num1 = 88
 num2 = 7
 result = sum_of_two_numbers(num1, num2)
-print(f" Сумма 2х  чисел  {num1} and {num2} is {result}")
+print(f" Сумма 2x cegth чисел  {num1} and {num2} is {result}")
